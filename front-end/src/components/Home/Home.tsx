856171fd881669032f8878429908500a43e55883
HuyenNguyen <nguyendieuhuyen2809@gmail.com>
2022-02-23 10:04:56 +0700

    Add animation to Home and Project Card components

--- front-end/src/components/Home/Home.tsx
+++ front-end/src/components/Home/Home.tsx
@@ -4,8 +4,47 @@ import { ProjectCard } from "../../lib/Card/ProjectCard";
 import { Header } from "../Header";
 import { NavBar } from "../NavBar";
 import "./home.scss";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export function Home () {
+    const [show, doShow] = useState({
+        itemOne: false,
+        itemTwo: false,
+        itemThree: false,
+        itemFour: false,
+        itemFive: false,
+    });
+    const refOne = useRef(null);
+    const refTwo = useRef(null);
+    const refThree = useRef(null);
+    const refFour = useRef(null);
+    const refFive = useRef(null);
+
+    useLayoutEffect(() => {
+        const topPos = (element : any) => element.getBoundingClientRect().top;
+
+        const div1Pos = topPos(refOne.current);
+        const div2Pos = topPos(refTwo.current);
+        const div3Pos = topPos(refThree.current);
+        const div4Pos = topPos(refFour.current);
+        const div5Pos = topPos(refFive.current);
+
+        const onScroll = () => {
+            const scrollPos = window.scrollY + window.innerHeight;
+            doShow((state) => ({
+                itemOne: div1Pos < scrollPos,
+                itemTwo: div2Pos < scrollPos,
+                itemThree: div3Pos < scrollPos,
+                itemFour: div4Pos < scrollPos,
+                itemFive: div5Pos < scrollPos,
+            }));
+        };
+
+        window.addEventListener("scroll", onScroll);
+        
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
     return(
         <>
         <NavBar/>
@@ -14,13 +53,17 @@ export function Home () {
             <section className="section-main-info">
                 <div className="section-main-info-aboutMe">
                     <h2 className="secondary-heading">About Me</h2>
-                    <p className="paragraph paragraph--animated">
+                    <p 
+                    className={`paragraph ${show.itemOne && "paragraph--animated"}`} 
+                    ref={refOne}>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                 </div>
                 <div className="section-main-info-porfolio">
                     <h2 className="secondary-heading">Portfolio</h2>
-                    <div className="section-main-info-porfolio-list animation-moveInLeft">
+                    <div 
+                    className={`section-main-info-porfolio-list ${show.itemTwo && "animation-moveInLeft"}`} 
+                    ref={refTwo}>
                         <div className="section-main-info-porfolio-list-image">
                             <img src="img/person-icon.jpg" alt="porfolio" className="porfolio-image"/>
                         </div>
@@ -37,7 +80,7 @@ export function Home () {
                 </div>
                 <div className="section-main-info-projects">
                     <h2 className="secondary-heading">Projects</h2>
-                    <div className="section-main-info-projects-list animation-moveInLeft">
+                    <div className={`section-main-info-projects-list`}>
                         <ProjectCard/>
                         <ProjectCard/>
                         <ProjectCard/>
@@ -45,15 +88,21 @@ export function Home () {
                 </div>
             </section>
             <section className="section-additional-info">
-                <div className="section-additional-info-button animation-moveInRight">
+                <div 
+                className={`section-additional-info-button ${show.itemOne && "animation-moveInRight"}`} 
+                ref={refThree}>
                     <BigButton/>
                 </div>
-                <div className="section-additional-info-feature-box animation-moveInRight">
+                <div 
+                className={`section-additional-info-feature-box ${show.itemTwo && "animation-moveInRight"}`} 
+                ref={refFour}>
                     <FeatureBox/>
                 </div>
                 <div className="section-additional-info-attachments">
                     <h2 className="secondary-heading">Attachments</h2>
-                    <div className="section-additional-info-attachments-list animation-moveInRight">
+                    <div 
+                    className={`section-additional-info-attachments-list ${show.itemTwo && "animation-moveInRight"}`}  
+                    ref={refFive}>
                         <button className="section-additional-info-attachments-list-item">
                             <span className="subtract-triangle">&nbsp;ab</span>
                             CV
--- front-end/src/lib/Card/ProjectCard.tsx
+++ front-end/src/lib/Card/ProjectCard.tsx
@@ -3,10 +3,34 @@ import { Rating } from "../Rating/Rating";
 import { SmallAvatar } from "../Avatar/SmallAvatar";
 import "./project-card.scss";
 import { SkillList } from "../SkillList/SkillList";
+import { useLayoutEffect, useRef, useState } from "react";
+
 
 export function ProjectCard () {
+    const [show, doShow] = useState({
+        card: false,
+    });
+    const refCard = useRef(null);
+
+    useLayoutEffect(() => {
+        const topPos = (element : any) => element.getBoundingClientRect().top;
+
+        const cardPos = topPos(refCard.current);
+
+        const onScroll = () => {
+            const scrollPos = window.scrollY + window.innerHeight;
+            doShow((state) => ({
+                card: cardPos < scrollPos,
+            }));
+        };
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
+
     return(
-        <div className="project-card">
+        <div className={`project-card ${show.card && "animation-moveInLeft"}`} ref={refCard}>
             <div className="project-card-info">
                 <h3 className="tertiary-heading">
                     IOS Developer
@@ -19,7 +43,7 @@ export function ProjectCard () {
                 <div className="project-card-info-developer">
                     <SmallAvatar textColor="black"/>
                     <div className="project-card-info-developer-date">
-                        &bullet;  
+                        &#8226;
                         <span>2021/04/06</span>
                     </div>
                 </div>
